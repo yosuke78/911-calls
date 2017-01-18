778b6eb656334c2f4b4dafe130c4097c51e266c7
Thomas SIMON <thomas.simon.work@gmail.com>
2017-01-18 15:53:46 +0100

    Import of the csv into elasticsearch

--- elasticsearch/import.js
+++ elasticsearch/import.js
@@ -7,11 +7,23 @@ var esClient = new elasticsearch.Client({
   log: 'error'
 });
 
+var arr = []
+var id = 0
 fs.createReadStream('../911.csv')
-    .pipe(csv())
-    .on('data', data => {
-      // TODO extract one line from CSV
-    })
-    .on('end', () => {
-      // TODO insert data to ES
-    });
+  .pipe(csv())
+  .on('data', data => {
+    arr.push(JSON.parse(JSON.stringify(data)))
+  })
+  .on('end', () => {
+    const bulkSize = 1000
+    for (let i = 0; i * bulkSize < arr.length; i++) {
+      let subArr = arr.slice(i * bulkSize,Math.min(arr.length, (i + 1) * bulkSize))
+      const body = []
+      subArr.forEach(x => {
+        body.push({ index: { _index: 'emergency', _type: 'csv', _id: id++ } })
+        body.push(x)
+      })
+      esClient.bulk({ body: body })
+    }
+  }
+  );
